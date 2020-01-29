b3b6ad855a7f006c0e5a71a6edf06e712c7892c4
pa1007 <pa.fourriere@pa1007.fr>
2020-01-29 13:56:42 +0100

    Refactor event

--- src/main/java/fr/pa1007/trobotframework/manager/EventManager.java
+++ src/main/java/fr/pa1007/trobotframework/manager/EventManager.java
@@ -2,39 +2,38 @@ package fr.pa1007.trobotframework.manager;
 
 import fr.pa1007.trobotframework.event.Event;
 import fr.pa1007.trobotframework.listener.Listener;
-
+import fr.pa1007.trobotframework.utils.Utils;
 import java.util.*;
 
+@SuppressWarnings({"unchecked", "rawtypes"})
 public class EventManager {
 
-    private static final Map<Class<? extends Event<?>>, List<Listener<?>>> listEvent =
+    private static final Map<Class<? extends Event<?>>, List<Listener>> listEvent =
             Collections.synchronizedMap(new HashMap<>());
 
     private EventManager() {
 
     }
 
-    public static void trigger(Event<?> event) {
+    public static <E extends Event<?>> void trigger(E event) {
         if (listEvent.containsKey(event.getClass())) {
-            for (Listener l : listEvent.get(event.getClass())) {
-                new Thread(() -> {
-                    l.listener(event);
-                }).start();
-            }
+            listEvent.get(event.getClass()).forEach((e) -> new Thread(() -> e.listener(event)).start());
+
         }
     }
 
-    public  static void addListener(Class<? extends Event<?>> event, Listener<?> listener) {
+    public static <E extends Event<?>, L extends Listener<E>> void addListener(Class<E> event, L listener) {
         if (listEvent.containsKey(event)) {
             listEvent.get(event).add(listener);
-        } else {
-            List<Listener<?>> list = Collections.synchronizedList(new ArrayList<>());
+        }
+        else {
+            List<Listener> list = Collections.synchronizedList(new ArrayList<>());
             list.add(listener);
             listEvent.put(event, list);
         }
     }
 
-    public static void removeListener(Event<?> event, Listener<?> listener) {
+    public static <E extends Event<?>, L extends Listener<E>> void removeListener(E event, L listener) {
         if (listEvent.containsKey(event.getClass())) {
             listEvent.get(event.getClass()).remove(listener);
         }
@@ -44,4 +43,20 @@ public class EventManager {
         listEvent.values().removeIf(value -> value.contains(listener));
     }
 
+    public static <E extends Event, L extends Listener> void forceTrigger(E event, L listener) {
+        new Thread(() -> {
+            try {
+                listener.listener(event);
+            }
+            catch (ClassCastException ex) {
+                Utils.getLogger().error("Error while forcing an event the event to the listener "
+                                        + listener.getClass().getName()
+                                        + "; \t Trying to send \""
+                                        + event.getClass().getName()
+                                        + "\" To a \""
+                                        + listener.getClass().getInterfaces()[0].getName()
+                                        + "\" Listener");
+            }
+        }).start();
+    }
 }
